8fcb2b1a9c4deca8d7bedf8b50078b32e6650daf
tthtung <142084562+tthtung@users.noreply.github.com>
2025-04-10 20:36:12 +0700

    Finish 1. Layout --> Grid css -->

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="custom-container">
+        <div className="header bg-white py-4 px-6 border-b border-gray-100 flex justify-between items-center">DASHBOARD</div>
+        <div className="menu border-r-1 border-gray-100">LOGO & MENU</div>
+        <div className="content">OVERVIEW</div>
+        <div className="table mt-6">DATATABLE</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
